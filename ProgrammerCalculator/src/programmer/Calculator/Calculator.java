b1c93a88d4190c00e8276bfe7f0c3ad9564557ba
Nathan <31359612+neerb@users.noreply.github.com>
2019-10-28 18:21:32 -0500

    Initial Creation of Project with main file and class

---
+++ ProgrammerCalculator/src/programmer/Calculator/Calculator.java
@@ -0,0 +1,23 @@
+package programmer.Calculator;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class Calculator extends JFrame implements ActionListener
+{
+	public Calculator()
+	{
+		setSize(400, 600);
+	
+		setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ ProgrammerCalculator/src/programmer/Calculator/CalculatorMain.java
@@ -0,0 +1,9 @@
+package programmer.Calculator;
+
+public class CalculatorMain 
+{
+	public static void main(String[] args)
+	{
+		Calculator calc = new Calculator();
+	}
+}
