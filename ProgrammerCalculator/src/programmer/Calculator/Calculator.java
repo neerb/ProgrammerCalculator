b65def5c59b0b7f5e63f286851793e3346d4d09f
Nathan <31359612+neerb@users.noreply.github.com>
2019-12-06 15:01:00 -0600

    Update Calculator.java

--- ProgrammerCalculator/src/programmer/Calculator/Calculator.java
+++ ProgrammerCalculator/src/programmer/Calculator/Calculator.java
@@ -9,9 +9,6 @@ import java.awt.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class Calculator extends JFrame implements ActionListener
 {	
